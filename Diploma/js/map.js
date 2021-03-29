5c180b3ca9c081a590fd98ffe119afffb2f643f2
Seredyukb <seredyuk.b@gmail.com>
2021-03-30 01:13:27 +0300

    git

--- Diploma/js/map.js
+++
@@ -1,8 +0,0 @@
-function myMap() {
-    var mapCanvas = document.getElementById("map");
-    var mapOptions = {
-        center: new google.maps.LatLng(53.929407548312845, 27.54697938179048), zoom: 10
-    };
-    var map = new google.maps.Map(mapCanvas, mapOptions);
-}		
-
