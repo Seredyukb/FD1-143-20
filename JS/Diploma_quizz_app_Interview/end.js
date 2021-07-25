1d52acad57e98bf186c4c6a8a18bce584d8ba7fd
Seredyukb <seredyuk.b@gmail.com>
2021-07-25 18:44:33 +0300

    done

--- JS/Diploma_quizz_app_Interview/end.js
+++ JS/Diploma_quizz_app_Interview/end.js
@@ -1,36 +1,42 @@
-const username = document.querySelector('#username')
-const saveScoreBtn = document.querySelector('#saveScoreBtn')
-const finalScore = document.querySelector('#finalScore')
-const mostRecentScore = localStorage.getItem('mostRecentScore')
+const username = document.querySelector('#username');
+const saveScoreBtn = document.querySelector('#saveScoreBtn');
+const finalScore = document.querySelector('#finalScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
 
-const highScores = JSON.parse(localStorage.getItem('highScores')) || []
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
-const MAX_HIGH_SCORES = 5
+const MAX_HIGH_SCORES = 5;
 
-finalScore.innerText = mostRecentScore
+if (mostRecentScore >= 2000) { finalScore.innerText = mostRecentScore + ' Ваши знания великолепны!' }
+else if (mostRecentScore >= 1500 && mostRecentScore <= 2000) {
+    finalScore.innerText = mostRecentScore + ' Ваши знания достаточно хороши, так держать!'
+} else if (mostRecentScore < 1500) {
+    finalScore.innerText = mostRecentScore + ' Не плохой результат, но хотелось бы лучше!'
+};
+
 
 username.addEventListener('keyup', () => {
-    saveScoreBtn.disabled = !username.value
+    saveScoreBtn.disabled = !username.value;
 })
 
 saveHighScore = e => {
-    e.preventDefault()
+    e.preventDefault();
 
     const score = {
         score: mostRecentScore,
         name: username.value
     }
 
-    highScores.push(score)
+    highScores.push(score);
 
     highScores.sort((a, b) => {
-        return b.score - a.score
-    })
+        return b.score - a.score;
+    });
 
-    highScores.splice(5)
+    highScores.splice(5);
 
-    localStorage.setItem('highScores', JSON.stringify(highScores))
-    window.location.assign('/')
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('/');
 
 
-}
+};
--- JS/Diploma_quizz_app_Interview/game.js
+++ JS/Diploma_quizz_app_Interview/game.js
@@ -6,6 +6,22 @@ const choices = Array.from(document.querySelectorAll('.choice-text'));
 const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#progressBarFull');
+var audio = document.getElementById('audio');
+var playPauseBTN = document.getElementById('playPauseBTN');
+var count = 0;
+
+function playPause() {
+    if (count == 0) {
+        count = 1;
+        audio.play();
+        playPauseBTN.innerHTML = "ВЫКЛ музыку ⏸";
+    } else {
+        count = 0;
+        audio.pause();
+        playPauseBTN.innerHTML = "ВКЛ музыку ►";
+    }
+
+}
 
 let currentQuestion = {};
 let acceptingAnswers = true;
@@ -49,7 +65,7 @@ let questions = [
         answer: 1,
     },
     {
-        question: "Что выведет код? Console.log(Number.isNan('string')); Console.log(isNaN('string'))",
+        question: "Что выведет код? \n Console.log(Number.isNan('string')); \n Console.log(isNaN('string'))",
         choice1: "true true",
         choice2: "false true",
         choice3: "true false",
@@ -103,71 +119,167 @@ let questions = [
         choice3: "if i <> 5",
         choice4: "if (i != 5)",
         answer: 4,
+    },
+    {
+        question: "Как правильно объявить массив?",
+        choice1: "var colors = 'red', 'green', 'blue'",
+        choice2: "var colors = 1 = ('red'), 2 = ('green'), 3 = ('blue')",
+        choice3: "var colors = (1:'red', 2:'green', 3:'blue')",
+        choice4: "var colors = ['red', 'green', 'blue']",
+        answer: 4,
+    },
+    {
+        question: "Как правильно объявить цикл for?",
+        choice1: "for (i <= 5; i++)",
+        choice2: "for i = 1 to 5",
+        choice3: "for (i = 0; i <= 5)",
+        choice4: "for (i = 0; i <= 5; i++)",
+        answer: 4,
+    },
+    {
+        question: "Что такое event.target?",
+        choice1: "элемент, на который мы 'навешиваем' событие",
+        choice2: "элемент, на котором произошло событие",
+        choice3: "элемент, который последним срабатывает при обработке события",
+        choice4: "элемент, который первым срабатывает при обработке события",
+        answer: 2,
+    },
+    {
+        question: "В каком варианте ответов все типы данных примитивные?",
+        choice1: "string, undefined, symbol",
+        choice2: "string, function",
+        choice3: "BigInt, number, Object",
+        choice4: "ни один из ответов не верен",
+        answer: 1,
+    },
+    {
+        question: "Что означает термин - поднятие (hoisting)?",
+        choice1: "термин обьясняет перемещение в начало вашего кода объявления переменной или функции физически",
+        choice2: "термин обьясняет передачу переменной в область памяти функции",
+        choice3: "термин, используемый для описания перемещения переменных и функций в начало их области (глобальной или функциональной), где мы определяем эту переменную или функцию",
+        choice4: "термин, используемый для описания перемещения переменных и функций в начальный файл с которого начинается выполнение кода",
+        answer: 3,
+    },
+    {
+        question: "Что верно про область видимости?",
+        choice1: "это область, где у нас есть действительный доступ к переменным или функциям",
+        choice2: "это область, где мы можем объявлять и вызывать только функции",
+        choice3: "в js существует два типа области видимости",
+        choice4: "все верно",
+        answer: 1,
+    },
+    {
+        question: "В чем разница между == и ===??",
+        choice1: "при == (абстрактное равенство) происходит приведение к одному типу данных слева направо",
+        choice2: "при === (строгое равенство) сравнивается по значению и типу без приведения типов, а при == (абстрактное равенство) только по значению",
+        choice3: "при == (абстрактное равенство) сравнивается по значениею, а === (строгое равенство) сравниваем по типу, но не значению",
+        choice4: "одно и то же, отличий нет как и в скобочках",
+        answer: 2,
+    },
+    {
+        question: "Что верно про свойство prototype в объектах?",
+        choice1: "оно хранит мета-данные про объект",
+        choice2: "это тоже самое что и this, т.е. записи this.a и someObj.prototype.a идентичны",
+        choice3: "это основна прототипного наследования в JavaScript",
+        choice4: "Obj.prototype = Obj.__proto__",
+        answer: 3,
+    },
+    {
+        question: "Что выведет в консоль? \n const set = new Set(['1','0','3','4','0','5',]); \n console.log(set.size)",
+        choice1: "4",
+        choice2: "5",
+        choice3: "6",
+        choice4: "undefined",
+        answer: 2,
+    },
+    {
+        question: "Что выведет в консоль? \n const o = {}; \n console.log(o.toString === Object.prototype.toString)",
+        choice1: "ничего не выведет, возникнет ошибка",
+        choice2: "true",
+        choice3: "false",
+        choice4: "NaN",
+        answer: 2,
+    },
+    {
+        question: "Что верно про async/await?",
+        choice1: "способ написания асинхронного или неблокирующего кода в JavaScript более читабельно",
+        choice2: "это вид асинхронных функций которые обрабатываются синхронно",
+        choice3: "это вид асинхронных функций которые возвращают результат в переданный колбэк",
+        choice4: "это способ объявления асинхронных функций",
+        answer: 1,
+    },
+    {
+        question: "Что такое процесс прокидывания события (Event Bubbling)?",
+        choice1: "это процесс когда событие передается ко всем родителям целевого элемента вплоть до window",
+        choice2: "привязка одного события сразу на несколько DOM-элементов вызов всех обработчиков одного события по стеку",
+        choice3: "вызов всех обработчиков одного события по стеку",
+        choice4: "Это не связано с событиями, это пузырбковый метод сортировки массива",
+        answer: 1,
     }
 ]
 
-const SCORE_POINTS = 100
-const MAX_QUESTIONS = 11
+const SCORE_POINTS = 100;
+const MAX_QUESTIONS = 23;
 
 startGame = () => {
     questionCounter = 0
     score = 0
     availableQuestions = [...questions]
     getNewQuestion()
-}
+};
 
 getNewQuestion = () => {
     if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score)
 
         return window.location.assign('end.html')
-    }
+    };
 
-    questionCounter++
-    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
-    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`
+    questionCounter++;
+    progressText.innerText = `Вопрос ${questionCounter} из ${MAX_QUESTIONS}`;
+    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
-    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
-    currentQuestion = availableQuestions[questionsIndex]
-    question.innerText = currentQuestion.question
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionsIndex];
+    question.innerText = currentQuestion.question;
 
     choices.forEach(choice => {
         const number = choice.dataset['number']
         choice.innerText = currentQuestion['choice' + number]
-    })
+    });
 
-    availableQuestions.splice(questionsIndex, 1)
+    availableQuestions.splice(questionsIndex, 1);
 
-    acceptingAnswers = true
-}
+    acceptingAnswers = true;
+};
 
 choices.forEach(choice => {
     choice.addEventListener('click', e => {
-        if (!acceptingAnswers) return
+        if (!acceptingAnswers) return;
 
-        acceptingAnswers = false
-        const selectedChoice = e.target
-        const selectedAnswer = selectedChoice.dataset['number']
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset['number'];
 
-        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
 
         if (classToApply === 'correct') {
-            incrementScore(SCORE_POINTS)
-        }
+            incrementScore(SCORE_POINTS);
+        };
 
-        selectedChoice.parentElement.classList.add(classToApply)
+        selectedChoice.parentElement.classList.add(classToApply);
 
         setTimeout(() => {
-            selectedChoice.parentElement.classList.remove(classToApply)
-            getNewQuestion()
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
 
-        }, 1000)
-    })
-})
+        }, 1000);
+    });
+});
 
 incrementScore = num => {
-    score += num
-    scoreText.innerText = score
-}
+    score += num;
+    scoreText.innerText = score;
+};
 
-startGame()
+startGame();
